4eea3f64bbf9d0a05bda150e19f0605049aac285
oskmkr <oskmkr@naver.com>
2014-12-18 20:45:22 +0900

    spring boot 임시 제거 & init configuration

--- Keywords/src/main/java/io/oskm/keywords/Application.java
+++ Keywords/src/main/java/io/oskm/keywords/Application.java
@@ -1,23 +1,23 @@
 
 package io.oskm.keywords;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-/**
- * http://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/htmlsingle/#getting-started-first-application-code
- * @author oskm
- *
- */
-@EnableAutoConfiguration
-@ComponentScan
-public class Application {
-
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
-	}
-}
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+///**
+// * http://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/htmlsingle/#getting-started-first-application-code
+// * @author oskm
+// *
+// */
+//@EnableAutoConfiguration
+//@ComponentScan
+//public class Application {
+//
+//	public static void main(String[] args) throws Exception {
+//		SpringApplication.run(Application.class, args);
+//	}
+//}
